46695017d0c160673848f2a907ff520fe90e1ee8
Denis <den1503777@gmail.com>
2019-04-24 16:09:25 +0300

    little changing

--- src/main/java/ru/myuniversity/admissionrest/repository/TestRepository.java
+++ src/main/java/ru/myuniversity/admissionrest/repository/TestRepository.java
@@ -1,7 +1,7 @@
 package ru.myuniversity.admissionrest.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import ru.myuniversity.admissionrest.entity.TestEntity;
 
-public interface TestRepository extends JpaRepository<TestEntity, Integer> {
+public interface TestRepository extends CrudRepository<TestEntity, Integer> {
 }
